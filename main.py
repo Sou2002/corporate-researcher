52b3f09cf1ed253071e4180e6422d895bbc7b79a
Sourav Banerjee <souravbnjee07@gmail.com>
2026-02-24 20:38:02 +0530

    Updated the chainlit ui

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import chainlit as cl
 from langchain.messages import HumanMessage
 from langgraph.config import RunnableConfig
-from src.agents.researcher.graph import create_researcher_graph
+from src.agents.research_manager.graph import create_reserach_manager_graph
 
 
 @cl.on_message
 async def on_message(msg: cl.Message):
 
-    config = {"configurable": {"thread_id": cl.context.session.id}}
+    # config = {"configurable": {"thread_id": cl.context.session.id}}
+    config = {}
     cb = cl.LangchainCallbackHandler()
     final_answer = cl.Message(content="")
 
-    graph = create_researcher_graph()
+    graph = create_reserach_manager_graph()
     
     for ai_msg, metadata in graph.stream(
         input={
@@ -23,7 +24,7 @@ async def on_message(msg: cl.Message):
         if (
             ai_msg.content
             and not isinstance(ai_msg, HumanMessage)
-            and metadata["langgraph_node"] == "research_manager"
+            # and metadata["langgraph_node"] == "research_manager"
         ):
             await final_answer.stream_token(ai_msg.content)
 
